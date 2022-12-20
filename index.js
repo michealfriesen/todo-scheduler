d44ad32e6eb332262c84871a867535655b6b227f
Micheal <michealfriesen@gmail.com>
2022-12-19 19:12:04 -0800

    Added basic packages, and started auth things

---
+++ index.js
@@ -0,0 +1,9 @@
+require("isomorphic-fetch") // or import the fetch polyfill you installed
+require("@microsoft/microsoft-graph-client")
+
+// Get auth
+
+
+// Try to get the tasklist
+
+console.log("Hello");
